e093a1ba075178c8bde88bee6c6058ecdc8498d0
YHM <hmyanghm@uno-solution.com>
2021-03-19 08:36:05 +0900

    210319 run test

---
+++ src/test/java/com/toy/api/springboot/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.toy.api.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello")) //MockMvc를 통해 /hello 주소로 HTTP GET 요청
+                .andExpect(status().isOk()) //Http Header의 status가 200이 아니면 에러
+                .andExpect(content().string(hello)); //HelloController에서 hello를 리턴하기 때문에 hello 씀
+    }
+}
