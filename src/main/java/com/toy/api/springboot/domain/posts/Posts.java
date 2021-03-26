38c7b842aced9ebb30807b6fd13dfc52282c2101
YHM <hmyanghm@uno-solution.com>
2021-03-26 17:28:38 +0900

    210326 repository test

---
+++ src/main/java/com/toy/api/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.toy.api.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //GenerationType.IDENTITY 이 옵션을 추가해야만 auto_increment가 됨
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
