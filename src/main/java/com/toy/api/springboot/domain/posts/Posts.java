2c956a44398f22ed9d9ebba63b70cbe2c6975680
YHM <hmyanghm@uno-solution.com>
2021-03-31 15:17:09 +0900

    210331 JPA Auditing으로 생성시간/수정시간 자동화하기

--- src/main/java/com/toy/api/springboot/domain/posts/Posts.java
+++ src/main/java/com/toy/api/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.toy.api.springboot.domain.posts;
 
+import com.toy.api.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //GenerationType.IDENTITY 이 옵션을 추가해야만 auto_increment가 됨
