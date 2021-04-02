ca252e2c640ec57a532359ccfa20761fc02555a3
YHM <hmyanghm@uno-solution.com>
2021-04-02 12:59:01 +0900

    210402 머스테치로 게시글 수정 화면 만들기

--- src/main/java/com/toy/api/springboot/web/IndexController.java
+++ src/main/java/com/toy/api/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.toy.api.springboot.web;
 
 import com.toy.api.springboot.service.posts.PostsService;
+import com.toy.api.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,6 +23,18 @@ public class IndexController {
 
     @GetMapping("/posts/save")
     public String postsSave() {
+
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+
+
 }
