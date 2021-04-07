8c06696e885bc68033ff4ee5e9208de57e411245
YHM <hmyanghm@uno-solution.com>
2021-04-07 15:25:31 +0900

    210407 스프링 시큐리티 로그인 기능 -> 어노테이션 기반으로 개선

--- src/main/java/com/toy/api/springboot/web/IndexController.java
+++ src/main/java/com/toy/api/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.toy.api.springboot.web;
 
+import com.toy.api.springboot.config.auth.LoginUser;
 import com.toy.api.springboot.config.auth.dto.SessionUser;
 import com.toy.api.springboot.service.posts.PostsService;
 import com.toy.api.springboot.web.dto.PostsResponseDto;
@@ -19,13 +20,14 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) { // Model은? 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
-                                       // +) postsService.findAllDesc()로 가져온 결과를 posts로 idex.mustache에 전달
+    public String index(Model model, @LoginUser SessionUser user) { // Model은? 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
+        // +) postsService.findAllDesc()로 가져온 결과를 posts로 idex.mustache에 전달
 
         model.addAttribute("posts", postsService.findAllDesc());
 
-        //CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        /*//CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
+        //이 부분은 @LoginUser를 사용해서 세션 정보를 가지고 올 수 있게 되어 필요 없게 됨
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");*/
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
